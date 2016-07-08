e8030e0d63ee2cdf27d63a412e5c2be7be0ff6c2
Erich Morisse <erich.morisse@gmail.com>
2016-07-08 15:37:44 -0400

    fixed an edge case where the item buffer overwrote itself, resulting in not all tasks getting listed

--- todoist.summary.py
+++ todoist.summary.py
@@ -5,41 +5,40 @@ import sys
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta, FR
 
-if datetime.today().weekday() == 4: # it's a friday
-   last_friday = -2
+if datetime.today().weekday() == 4: # today is Fri, so we need previous Fri
+	last_friday = -2
 else:
-   last_friday = -1
+	last_friday = -1
 
 since = datetime.now() + relativedelta(weekday=FR(last_friday))
 since = since.strftime("%Y-%m-%dT00:01")
 
+#since = "2016-06-24T00:01"
+
 datecount = {}
 
-user = 'username'
-password = 'password'
-
-api = todoist.TodoistAPI()
-user = api.login(user,password)
+api = todoist.TodoistAPI('put your API key here!')
 
 offset = 0
 names = {}
 items = {}
-response = api.get_all_completed_items(since=since,offset=offset)
+response = api.get_all_completed_items(since=since)
 
 while len(response['items']) > 0:
 	for p in response['projects']:
 		names[p] = response['projects'][p]['name']
-		items[p] = []
+		if not p in items:
+			items[p] = []
 	
 	for i in response['items']:
 		project = str(i['project_id'])
 		items[project].append(i['content'])
+		offset += 1
 	
-	offset += len(response['items'])
 	response = api.get_all_completed_items(since=since,offset=offset)
 
-for p in items:
-	print "# ", names[str(p)].encode('ascii', 'xmlcharrefreplace')
+for p in names:
+	print "# ", names[p].encode('ascii', 'xmlcharrefreplace')
 	for i in items[p]:
 		print "* ", i.encode('ascii', 'xmlcharrefreplace')
 	print
